8f9afae2825d29649890e77169ab6382b0ca5154
Onur Altıntaş <onur.altintas2045@gmail.com>
2026-02-16 21:01:27 +0300

    Project created.

---
+++ Paycell/Paycell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Paycell
+//
+//  Created by Ebru Bircan on 16.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
